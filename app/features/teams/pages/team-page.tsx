db4b4e43a83f46b644fc152f026f411e67c2469d
JHoo0118 <kik3078@gmail.com>
2025-03-09 16:02:25 +0900

    Teams Page

---
+++ app/features/teams/pages/team-page.tsx
@@ -0,0 +1,8 @@
+import type { Route } from "./+types/team-page";
+
+export const meta: Route.MetaFunction = () => [
+  { title: "Team Details | wemake" },
+];
+export default function TeamPage() {
+  return <div></div>;
+}
