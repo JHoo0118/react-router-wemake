a63a5d1e8e97ec75e474794d0005f2a42434775d
JHoo0118 <kik3078@gmail.com>
2025-03-13 21:42:54 +0900

    Suspense and Await

--- app/features/community/queries.ts
+++ app/features/community/queries.ts
@@ -43,6 +43,7 @@ import client from "~/supa-client";
 // };
 
 export const getTopics = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   const { data, error } = await client.from("topics").select("name, slug");
   if (error) throw new Error(error.message);
   return data;
@@ -65,6 +66,7 @@ export const getPosts = async () => {
   //       count
   //     )
   // `);
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   const { data, error } = await client
     .from("community_post_list_view")
     .select(`*`);
